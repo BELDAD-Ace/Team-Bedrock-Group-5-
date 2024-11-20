79698c10b1dd53d1d29b9126d923b714c19b7938
Beldad <Beldadj48@gmail.com>
2024-11-20 20:41:44 +0800

    DEL

--- Jscript/Core.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-    const loadingS = document.querySelector (".loadingS");
-
-    loadingS.classList.add("loadingS--END");
-
-    loadingS.addEventListener("transitionend", () => {
-            document.body.removeChild(loadingS);
-    });
-});
